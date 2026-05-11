5bd45caa03b03d59c9f6d8b6e123f0957233bd60
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-05-10 23:24:50 -0400

    añadir health para mantener render arriba

---
+++ src/main/java/com/sanosysalvos/gestionmascotas/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.sanosysalvos.gestionmascotas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "APi arribaaaa";
+    }
+    
+}
